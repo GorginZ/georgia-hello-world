1d135df4a24ef4091294739729c9b6ea62f4aafe
GorginZ <georgia.z.leng@gmail.com>
2022-08-13 21:01:45 +1000

    only build on main

--- bin/build_and_publish.sh
+++ bin/build_and_publish.sh
@@ -18,4 +18,4 @@ description=$(cat envs/$env-description.txt)
 
 docker build --build-arg version="$version" --build-arg sha="$commit" --build-arg description="$description" -t "ghcr.io/gorginz/georgia-hello-world:$commit" .
 
-# docker push "ghcr.io/gorginz/georgia-hello-world:$commit"
+docker push "ghcr.io/gorginz/georgia-hello-world:$commit"
