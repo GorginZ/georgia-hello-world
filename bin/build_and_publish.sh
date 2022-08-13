67ac06fc71458238e20b74fca1f3880f08cda366
GorginZ <georgia.z.leng@gmail.com>
2022-08-13 20:39:30 +1000

    hmm is it not waiting for the cmd substitution? I really probably shouldn't use yq here ugh

--- bin/build_and_publish.sh
+++ bin/build_and_publish.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -ex
 
-# die() { echo "$1" >&2; exit "${2:-1}"; }
+die() { echo "$1" >&2; exit "${2:-1}"; }
 
 if [[ "$#" != 1 ]]; then
   echo "usage: $0 <development-environment>"
   exit 245
 fi
 
-# hash docker || die "docker not found"
+hash docker || die "docker not found"
 
 env=$1
 
@@ -16,9 +16,11 @@ commit=$(git describe --tags --always)
 version=$(cat VERSION)
 description=$(docker compose run -i --rm yq '.description' envs/$env.yaml)
 
+wait
+
 echo $description
 echo "works on my machine!"
 
-# docker build --build-arg version="$version" --build-arg sha="$commit" --build-arg description="$description" -t "ghcr.io/gorginz/georgia-hello-world:$commit" .
+docker build --build-arg version="$version" --build-arg sha="$commit" --build-arg description="$description" -t "ghcr.io/gorginz/georgia-hello-world:$commit" .
 
-# docker push "ghcr.io/gorginz/georgia-hello-world:$commit"
+docker push "ghcr.io/gorginz/georgia-hello-world:$commit"
