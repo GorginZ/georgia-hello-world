5341f555acdd4ef69510f5c5cd98cc5d48e37a04
GorginZ <georgia.z.leng@gmail.com>
2022-08-13 20:23:47 +1000

    dire straits let's see what's goign wrong with this sub cmd

--- bin/build_and_publish.sh
+++ bin/build_and_publish.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -ex
 
-die() { echo "$1" >&2; exit "${2:-1}"; }
+# die() { echo "$1" >&2; exit "${2:-1}"; }
 
 if [[ "$#" != 1 ]]; then
   echo "usage: $0 <development-environment>"
   exit 245
 fi
 
-hash docker || die "docker not found"
+# hash docker || die "docker not found"
 
 env=$1
 
@@ -19,6 +19,6 @@ description=$(docker compose run -i --rm yq '.description' envs/$env.yaml)
 echo $description
 echo "works on my machine!"
 
-docker build --build-arg version="$version" --build-arg sha="$commit" --build-arg description="$description" -t "ghcr.io/gorginz/georgia-hello-world:$commit" .
+# docker build --build-arg version="$version" --build-arg sha="$commit" --build-arg description="$description" -t "ghcr.io/gorginz/georgia-hello-world:$commit" .
 
 # docker push "ghcr.io/gorginz/georgia-hello-world:$commit"
