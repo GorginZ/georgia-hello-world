4c53a70ffb157abc39d6230dc76843a3f13e30e7
GorginZ <georgia.z.leng@gmail.com>
2022-08-13 20:09:56 +1000

    put back docker hash and die and echo description bc this .sh 'works on my machine'

--- bin/build_and_publish.sh
+++ bin/build_and_publish.sh
@@ -8,12 +8,17 @@ if [[ "$#" != 1 ]]; then
   exit 245
 fi
 
+hash docker || die "docker not found"
+
 env=$1
 
 commit=$(git describe --tags --always)
 version=$(cat VERSION)
 description=$(docker compose run -i --rm yq '.description' envs/$env.yaml)
 
+echo $description
+echo "works on my machine!"
+
 docker build --build-arg version="$version" --build-arg sha="$commit" --build-arg description="$description" -t "ghcr.io/gorginz/georgia-hello-world:$commit" .
 
-docker push "ghcr.io/gorginz/georgia-hello-world:$commit"
+# docker push "ghcr.io/gorginz/georgia-hello-world:$commit"
