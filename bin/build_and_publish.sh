fcb7d84ddc1be62f161f638eb6b0dfa92411c15e
GorginZ <georgia.z.leng@gmail.com>
2022-08-13 20:58:33 +1000

    close the sub command duh-oh

--- bin/build_and_publish.sh
+++ bin/build_and_publish.sh
@@ -14,8 +14,8 @@ env=$1
 
 commit=$(git describe --tags --always)
 version=$(cat VERSION)
-description=$(cat envs/$env-description.txt
+description=$(cat envs/$env-description.txt)
 
 docker build --build-arg version="$version" --build-arg sha="$commit" --build-arg description="$description" -t "ghcr.io/gorginz/georgia-hello-world:$commit" .
 
-docker push "ghcr.io/gorginz/georgia-hello-world:$commit"
+# docker push "ghcr.io/gorginz/georgia-hello-world:$commit"
