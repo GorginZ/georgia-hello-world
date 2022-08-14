e2f99389200303f4b62706694cf76add4f46ebde
GorginZ <georgia.z.leng@gmail.com>
2022-08-14 12:04:41 +1000

    tag with version, tag is optional argumen to allow buildin test image easier

--- bin/build_and_publish.sh
+++ bin/build_and_publish.sh
@@ -3,8 +3,9 @@ set -euxo pipefail
 
 die() { echo "$1" >&2; exit "${2:-1}"; }
 
-if [[ "$#" != 1 ]]; then
-  echo "usage: $0 <development-environment>"
+if [[ "$#" < 1 ]]; then
+  echo "usage: $0 <development-environment> <tag>"
+  echo "tag is optional"
   exit 245
 fi
 
@@ -15,7 +16,8 @@ env=$1
 commit=$(git describe --tags --always)
 version=$(cat VERSION)
 description=$(cat envs/$env-description.txt)
+tag=${2:-$version}
 
-docker build --build-arg version="$version" --build-arg sha="$commit" --build-arg description="$description" -t "ghcr.io/gorginz/georgia-hello-world:$commit" .
+docker build --build-arg version="$version" --build-arg sha="$commit" --build-arg description="$description" -t "ghcr.io/gorginz/georgia-hello-world:$tag" .
 
-docker push "ghcr.io/gorginz/georgia-hello-world:$commit"
+docker push "ghcr.io/gorginz/georgia-hello-world:$tag"
